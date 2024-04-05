2e72aebc71a17dd89c17dd67bf8289cb1f52ccb6
hariom <hariom987316@gmail.com>
2024-04-05 14:48:31 +0530

    adding java project

---
+++ notes.js
@@ -0,0 +1,44 @@
+/**
+ # Execution of java Code
+   - test.java Code (use command [javac test.java]).
+   - test.class (java complied code along with byte code).
+   - Execute using [java test]
+
+ # Variable
+   - number Storing
+     ~ int - Store the whole number like 1,5,10,10201.
+     ~ double - Store the decimal number along with very small and very large number. like 1.797,693,134,862,315,7 E+308
+   - Boolean
+     ~ boolean - Store the boolean value true on false.
+   - Char
+     ~ char - Store the only single digit value like 'H','A','R'
+   - String
+     ~ String - Store the strings value "Hariom Gola"
+
+ # Operators
+   - +,-,*,/,%
+ # Compound assigment operator
+   - Addition (+=)
+   - Subtraction (-=)
+   - Multiplication (*=)
+   - Division (/=)
+   - Modulo (%=)
+ # Greater than and less than opertaor
+   - > (Greater)
+   - < (Less)
+   - == (Equals)
+   - != (Not Equals)
+
+ # Final
+  - If we define any keyword as final it cannot be change like.
+    final String name = "Hariom Gola";
+
+
+
+
+
+
+
+
+
+ */
