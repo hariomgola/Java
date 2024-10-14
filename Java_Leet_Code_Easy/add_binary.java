b7509ccfb233fb3c447b533bf3f3b2f2fc6af1a9
hariom <hariom987316@gmail.com>
2024-10-15 00:59:59 +0530

    [Adding leet code easy]

--- Java_Leet_Code_Easy/add_binary.java
+++ Java_Leet_Code_Easy/add_binary.java
@@ -4,46 +4,42 @@ public class add_binary {
 
 	public static void main(String[] args) {
 		add_binary cs = new add_binary();
-		cs.addBinary("11", "1");
-		cs.addBinary("1000110", "11"); // 70 // 3 // 1001001 - reverse - 1001001
+		cs.addBinary("11", "1"); // 100
+		cs.addBinary("1000110", "11"); // 1001001
 		cs.addBinary("110", "111"); // 1101
-		cs.addBinary("100001", "10010"); // 11001
+		cs.addBinary("100001", "10010"); // 110011
 		cs.addBinary("110", "11"); // 1001
-		cs.addBinary("10100000100100110110010000010101111011011001101110111111111101000000101111001110", "111");
+		cs.addBinary("10100000100100110110010000010101111011011001101110111111111101000000101111001110", "111"); // 10100000100100110110010000010101111011011001101110111111111101000000101111010101
 	}
 
 	public String addBinary(String a, String b) {
 		a = new StringBuffer(a).reverse().toString();
 		b = new StringBuffer(b).reverse().toString();
-		String _longer = a.length() > b.length() ? a : b;
-
+		int lengthValue = a.length() >= b.length() ? b.length() : a.length();
+		String stringValue = a.length() > b.length() ? a.substring(lengthValue) : b.substring(lengthValue);
+		int carryValue = 0;
 		String _final = "";
-		int _temp = 0;
 
-		// Logic for iterating the value with length same
-		for (int i = 0; i < (a.length() >= b.length() ? b.length() : a.length()); i++) {
-			int _localTemp = Character.getNumericValue(a.charAt(i)) + Character.getNumericValue(b.charAt(i) + _temp);
-			if (_localTemp > 0) {
-				if (_localTemp > 1) {
-					if (_localTemp > 2) {
-						_final += 1;
-						_temp = 1;
-					} else {
-						_final += 0;
-						_temp = 1;
-					}
-				} else {
-					_final += 1;
-					_temp = 0;
-				}
-			} else {
-				_final += 0;
-				_temp = 0;
+		for (int i = 0; i < lengthValue; i++) {
+			int _temp = Character.getNumericValue(a.charAt(i)) + Character.getNumericValue(b.charAt(i)) + carryValue;
+			carryValue = _temp > 2 ? 1 : _temp > 1 ? 1 : 0;
+			_final += _temp > 2 ? 1 : _temp > 1 ? 0 : _temp > 0 ? 1 : 0;
+		}
+
+		if (stringValue.length() > 0) {
+			for (int i = 0; i < stringValue.length(); i++) {
+				int _temp = Character.getNumericValue(stringValue.charAt(i)) + carryValue;
+				carryValue = _temp > 2 ? 1 : _temp > 1 ? 1 : 0;
+				_final += _temp > 2 ? 1 : _temp > 1 ? 0 : _temp > 0 ? 1 : 0;
 			}
 		}
 
-		System.out.println("Final Result - " + new StringBuffer(_final).reverse() + "\n");
-		return "";
+		if (carryValue > 0) {
+			_final += carryValue > 0 ? 1 : "";
+		}
+
+		System.out.println("Final Result - " + new StringBuffer(_final).reverse());
+		return new StringBuffer(_final).reverse().toString();
 	}
 
 }
---
+++ Java_Leet_Code_Easy/sqrtx.java
@@ -0,0 +1,15 @@
+package Java_Leet_Code_Easy;
+
+public class sqrtx {
+
+	public static void main(String[] args) {
+		sqrtx cs = new sqrtx();
+		cs.mySqrt(4);
+		cs.mySqrt(8);
+
+	}
+
+	public int mySqrt(int x) {
+		return 0;
+	}
+}
