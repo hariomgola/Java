03f2a80a8308facfbbd540acd079fb0b587c255a
hariom <hariom987316@gmail.com>
2024-10-15 01:13:01 +0530

    [Adding Recurssion problem]

--- Java_HackerRank/Java_Stdin_and_Stdout_I.java
+++
@@ -1,7 +0,0 @@
-package Java_HackerRank;
-
-public class Java_Stdin_and_Stdout_I {
-	public static void main(String[] args) {
-
-	}
-}
---
+++ Java_Leet_Code_Easy/climbing_stairs.java
@@ -0,0 +1,14 @@
+package Java_Leet_Code_Easy;
+
+// Google
+public class climbing_stairs {
+	public static void main(String[] args) {
+		climbing_stairs cs = new climbing_stairs();
+		cs.climbStairs(2);
+		cs.climbStairs(3);
+	}
+
+	public int climbStairs(int n) {
+		return 0;
+	}
+}
