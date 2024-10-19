eab355e76a2b7ac18200fa06cfe03f6ff2ba4ac4
hariom <hariom987316@gmail.com>
2024-10-19 23:45:30 +0530

    [Adding leet code Easy]

---
+++ Java_Leet_Code_Easy/_83_remove_duplicates_from_sorted_list.java
@@ -0,0 +1,14 @@
+package Java_Leet_Code_Easy;
+
+public class _83_remove_duplicates_from_sorted_list {
+
+	public static void main(String[] args) {
+		_83_remove_duplicates_from_sorted_list cs = new _83_remove_duplicates_from_sorted_list();
+
+	}
+
+	public ListNode deleteDuplicates(ListNode head) {
+
+	}
+
+}
--- Java_Leet_Code_Easy/climbing_stairs.java
+++ Java_Leet_Code_Easy/climbing_stairs.java
@@ -4,8 +4,10 @@ package Java_Leet_Code_Easy;
 public class climbing_stairs {
 	public static void main(String[] args) {
 		climbing_stairs cs = new climbing_stairs();
-		cs.climbStairs(2);
-		cs.climbStairs(3);
+		cs.climbStairs(1); // [1]
+		cs.climbStairs(2); // [1,1] - [2]
+		cs.climbStairs(3); // [1,1,1] - [1,2] - [2,1]
+		cs.climbStairs(4); // [1,1,1,1] - [2,2] - [1,1,2] - [2,1,1] - [1,2,1]
 	}
 
 	public int climbStairs(int n) {
