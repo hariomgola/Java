7c586ef32d86eb68934a26dbebbd627612358220
hariom <hariom987316@gmail.com>
2024-10-20 00:09:31 +0530

    [Adding leetcode linkedlist]

--- Java_Leet_Code_Easy/_83_remove_duplicates_from_sorted_list.java
+++ Java_Leet_Code_Easy/_83_remove_duplicates_from_sorted_list.java
@@ -2,13 +2,24 @@ package Java_Leet_Code_Easy;
 
 public class _83_remove_duplicates_from_sorted_list {
 
+	class ListNode {
+		int val;
+		ListNode next;
+	}
+
 	public static void main(String[] args) {
 		_83_remove_duplicates_from_sorted_list cs = new _83_remove_duplicates_from_sorted_list();
-
 	}
 
 	public ListNode deleteDuplicates(ListNode head) {
-
+		ListNode currentNode = head;
+		while (currentNode != null) {
+			if (currentNode.next != null && currentNode.val == currentNode.next.val) {
+				currentNode.next = currentNode.next.next;
+			} else {
+				currentNode = currentNode.next;
+			}
+		}
+		return head;
 	}
-
 }
