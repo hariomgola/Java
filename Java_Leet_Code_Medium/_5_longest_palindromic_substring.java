546db2c47d2cee2f07c8042acca8b79bd7b3ca66
hariom <hariom987316@gmail.com>
2024-10-20 15:57:57 +0530

    [Adding leet code medium]

--- Java_Leet_Code_Medium/_5_longest_palindromic_substring.java
+++ Java_Leet_Code_Medium/_5_longest_palindromic_substring.java
@@ -3,12 +3,93 @@ package Java_Leet_Code_Medium;
 public class _5_longest_palindromic_substring {
 
 	public static void main(String[] args) {
+		long startTime;
 		_5_longest_palindromic_substring cs = new _5_longest_palindromic_substring();
+
+		// Ts1
+		startTime = System.nanoTime();
 		cs.longestPalindrome("babad");
+		System.out.println("Execution Time - " + (System.nanoTime() - startTime) / 1000000 + "\n");
+		System.exit(0);
+		// Ts2
+		startTime = System.nanoTime();
+		cs.longestPalindrome("cbbd");
+		System.out.println("Execution Time - " + (System.nanoTime() - startTime) / 1000000 + "\n");
+		// Ts3
+		startTime = System.nanoTime();
+		cs.longestPalindrome("a");
+		cs.longestPalindrome("abacab");
+		System.out.println("Execution Time - " + (System.nanoTime() - startTime) / 1000000 + "\n");
+		// Ts4
+		startTime = System.nanoTime();
+		cs.longestPalindrome(
+				"vvgogaewginhopuxzwyryobjjpieyhwfopiowxmyylvcgsnhfcnogpqpukzmnpewavoutbloyrrhatimmxfqmcwgfebuoqbbgvubbkjfvxivjfijlpvtsnhagzfptahhyojwzamayoiegkenycnkxzkambimhdykdcxyyfjnvztzypmfczdhhnkmfuvgkhzbwmjznykkagqdrueohgcmeidjqsvfugcioeduohprjtfdmtzosnhoiganffarokxjifzzxhixdzycwfheqqegduzwtiacmdhqfmxhazcxsqyrvrihfqzjxvawdeandnwgjlquvzadruiqmcsgibglhicsvzqknztqpkiihdoisxipkourentfvrltieihiktgzswtgcmmlfrjifqinhrbplbsgswqlbjkyxjwoshsvxlhlpgzwbmxzwaemtowcxwourjwmmwjruowxcwotmeawzxmbwzgplhlxvshsowjxykjblqwsgsblpbrhniqfijrflmmcgtwszgtkihieitlrvftneruokpixsiodhiikpqtznkqzvscihlgbigscmqiurdazvuqljgwndnaedwavxjzqfhirvryqsxczahxmfqhdmcaitwzudgeqqehfwcyzdxihxzzfijxkoraffnagiohnsoztmdftjrphoudeoicgufvsqjdiemcghoeurdqgakkynzjmwbzhkgvufmknhhdzcfmpyztzvnjfyyxcdkydhmibmakzxkncynekgeioyamazwjoyhhatpfzgahnstvpljifjvixvfjkbbuvgbbqoubefgwcmqfxmmitahrryolbtuovawepnmzkupqpgoncfhnsgcvlyymxwoipofwhyeipjjboyrywzxupohnigweagogvv");
+		System.out.println("Execution Time - " + (System.nanoTime() - startTime) / 1000000 + "\n");
+
+		// Ts5
+		startTime = System.nanoTime();
+		cs.longestPalindrome(
+				"qbmhukucteihghldwdobtvgwwnhflpceiwhbkmvxavmqxedfndegztlpjptpdowwavemasyrjxxnhldnloyizyxgqlhejsdylvkpdzllrzoywfkcamhljminikvwwvqlerdilrdgzifojjlgeayprejhaequyhcohoeonagsmfrqhfzllobwjhxdxzadwxiglvzwiqyzlnamqqsastxlojpcsleohgtcuzzrvwzqugyimaqtorkafyebrgmrfmczwiexdzcokbqymnzigifbqzvfzjcjuugdmvegnvkgbmdowpacyspszvgdapklrhlhcmwkwwqatfswmxyfnxkepdotnvwndjrcclvewomyniaefhhcqkefkyovqxyswqpnysafnydbiuanqphfhfbfovxuezlovokrsocdqrqfzbqhntjafzfjisexcdlnjbhwrvnyabjbshqsxnaxhvtmqlfgdumtpeqzyuvmbkvmmdtywquydontkugdayjqewcgtyajofmbpdmykqobcxgqivmpzmhhcqiyleqitojrrsknhwepoxawpsxcbtsvagybnghqnlpcxlnshihcjdjxxjjwyplnemojhodksckmqdvnzewhzzuswzctnlyvyttuhlreynuternbqonlsfuchxtsyhqlvifcxerzqepthwqrsftaquzuxwcmjjulvjrkatlfqshpyjsbaqwawgpabkkjrtchqmriykbdsxwnkpaktrvmxjtfhwpzmieuqevlodtroiulzgbocrtiuvpywtcxvajkpfmaqckgrcmofkxynjxgvxqvkmhdbvumdaprijkjxxvpqnxakiavuwnifvyfolwlekptxbnctjijppickuqhguvtoqfgepcufbiysfljgmfepwyaxusvnsratn");
+		System.out.println("Execution Time - " + (System.nanoTime() - startTime) / 1000000 + "\n");
+		// Ts6
+		startTime = System.nanoTime();
+		cs.longestPalindrome(
+				"busislnescsicxpvvysuqgcudefrfjbwwjcchtgqyajdfwvkypfwshnihjdztgmyuuljxgvhdiwphrweyfkbnjgerkmifbirubhseuhrugwrabnjafnbdfjnufdstjbkuwtnpflffaqmjbhssjlnqftgjiglvvequhapasarlkcvbmkwnkuvwktbgfoaxteprobdwswcdyddyvrehvmxrrjiiidatidlpihkbmmruysmhhsncmfdanafdrfpdtfgkglcqpwrrtvacuicohspkounojuziittugpqjyhhkwfnflozbispehrtrnizowrlzcuollagxwtznjwzcumvedjwokueuqktvvouwnsmpxqvvpuwprezrbobrpnwaccwljchdguubjulyilzvmandjjleitweybqkjttschrjjlebnmponvlktzzcdtuybugggcqffkcffpamauvxfbonjrobgpvlyzveiwemmtdvbjciaytvesnocnjrwodtcokgcuoiicxapmrzpkfphjniuvzjrhbnqndfshoduejyktebgdabidxlkstepuwvtrtgbxaeheylicvhrxddijshcvdadxzsccmainyfpfdhqdanfccqkzlmhsfilvoybqojlvbcixjzqpbngdvesuokbxhkomsiqfyukvspqthlzxdnlwthrgaxhtpjzhrugqbfokrdcyurivmzgtynoqfjbafboselxnfupnpqlryvlcxeksirvufepfwczosrrjpudbwqxwldgjyfjhzlzcojxyqjyxxiqvfhjdwtgoqbyeocffnyxhyyiqspnvrpxmrtcnviukrjvpavervvztoxajriuvxqveqsrttjqepvvahywuzwtmgyrzduxfqspeipimyoxmkadrvrdyefekjxcmsmzmtbugyckcbjsrymszftjyllfmoeoylzeahnrxlxpnlvlvzltwnmldi");
+		System.out.println("Execution Time - " + (System.nanoTime() - startTime) / 1000000 + "\n");
+		// Ts7
+		startTime = System.nanoTime();
+		cs.longestPalindrome(
+				"wsgdzojcrxtfqcfkhhcuxxnbwtxzkkeunmpdsqfvgfjhusholnwrhmzexhfqppatkexuzdllrbaxygmovqwfvmmbvuuctcwxhrmepxmnxlxdkyzfsqypuroxdczuilbjypnirljxfgpuhhgusflhalorkcvqfknnkqyprxlwmakqszsdqnfovptsgbppvejvukbxaybccxzeqcjhmnexlaafmycwopxntuisxcitxdbarsicvwrvjmxsapmhbbnuivzhkgcrshokkioagwidhmfzjwwywastecjsolxmhfnmgommkoimiwlgwxxdsxhuwwjhpxxgmeuzhdzmuqhmhnfwwokgvwsznfcoxbferdonrexzanpymxtfojodcfydedlxmxeffhwjeegqnagoqlwwdctbqnuxngrgovrjesrkjrfjawknbrsfywljscfvnjhczhyeoyzrtbkvvfvofykkwoiclgxyaddhpdoztdhcbauaagjmfzkkdqexkczfsztckdlujgqzjyuittnudpldjvsbwbzcsazjpxrwfafievenvuetzcxynnmskoytgznvqdlkhaowjtetveahpjguiowkiuvikwewmgxhgfjuzkgrkqhmxxavbriftadtogmhlatczusxkktcsyrnwjbeshifzbykqibghmmvecwwtwdcscikyzyiqlgwzycptlxiwfaigyhrlgtjocvajcnqyenxrnjgogeqtvkxllxpuoxargzgcsfwavwbnktchwjebvwwhfghqkcjhuhuqwcdsixrkfjxuzvhjxlyoxswdlwfytgbtqbeimzzogzrlovcdpseoafuxfmrhdswwictsctawjanvoafvzqanvhaohgndbsxlzuymvfflyswnkvpsvqezekeidadatsymbvgwobdrixisknqpehddjrsntkqpsfxictqbnedjmsveurvrtvpvzbengdijkfcogpcrvwyf");
+		System.out.println("Execution Time - " + (System.nanoTime() - startTime) / 1000000 + "\n");
 	}
 
+	// checking small string first
+	public String _longestPalindrome(String s) {
+		int stringLength = s.length();
+		String palindrome = "";
+		outerLoop: for (int i = 0; i < stringLength; i++) {
+			for (int j = i; j < stringLength; j++) {
+				String _temp = s.substring(i, j + 1);
+				if (_temp.length() > palindrome.length() && checkPalindrome(_temp)) {
+					palindrome = _temp;
+					if (_temp.length() == stringLength) {
+						break outerLoop;
+					}
+				}
+			}
+
+		}
+		System.out.println("Final - " + palindrome);
+		return palindrome;
+	}
+
+	// checking big string first
 	public String longestPalindrome(String s) {
-		return "";
+		int stringLength = s.length();
+		String _finalValue = "";
+		outerLoop: for (int i = stringLength; i > 0; i--) {
+			int _temp = stringLength - i;
+			for (int j = stringLength; j >= i; j--) {
+				if (checkPalindrome(s.substring(_temp, j))) {
+					_finalValue = s.substring(_temp, j);
+					break outerLoop;
+				}
+				_temp -= 1;
+			}
+		}
+		return _finalValue;
+	}
+
+	public boolean checkPalindrome(String s) {
+		boolean _finalTest = true;
+		int stringLength = s.length();
+		for (int i = 0; i < stringLength; i++) {
+			if (s.charAt(i) != s.charAt(stringLength - i - 1)) {
+				_finalTest = false;
+				break;
+			}
+		}
+		return _finalTest;
 	}
 
 }
