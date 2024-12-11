f580f16ea3bdb49181fd411545067f276f72b575
hariom <hariom987316@gmail.com>
2024-12-11 09:56:03 +0530

    [Adding leetcode]

---
+++ Java_Leet_Code_Medium/_2779_maximum_beauty_of_an_array_after_applying_operation.java
@@ -0,0 +1,14 @@
+package Java_Leet_Code_Medium;
+
+public class _2779_maximum_beauty_of_an_array_after_applying_operation {
+
+	public static void main(String[] args) {
+		_2779_maximum_beauty_of_an_array_after_applying_operation cs = new _2779_maximum_beauty_of_an_array_after_applying_operation();
+		cs.maximumBeauty(new int[] { 4, 6, 1, 2 }, 2);
+	}
+
+	public int maximumBeauty(int[] nums, int k) {
+		return 0;
+	}
+
+}
