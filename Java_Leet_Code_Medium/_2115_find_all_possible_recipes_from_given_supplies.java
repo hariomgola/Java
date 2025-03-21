4b529e8227cfad418b75ca2e739093e459b3e1fa
hariom <hariom987316@gmail.com>
2025-03-21 13:26:57 +0530

    [Adding leet]

---
+++ Java_Leet_Code_Medium/_2115_find_all_possible_recipes_from_given_supplies.java
@@ -0,0 +1,22 @@
+package Java_Leet_Code_Medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _2115_find_all_possible_recipes_from_given_supplies {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		_2115_find_all_possible_recipes_from_given_supplies cs = new _2115_find_all_possible_recipes_from_given_supplies();
+		// calling the test cases here
+		cs.findAllRecipes(new String[] { "bread" }, List.of(List.of("yeast", "flour")),
+				new String[] { "yeast", "flour", "corn" });
+
+	}
+
+	public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
+		List<String> ls = new ArrayList<String>();
+		return ls;
+	}
+
+}
