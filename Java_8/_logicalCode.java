b1de18a17a2d4d05510f5d0aab9c3a57cd1c8b94
hariom <hariom987316@gmail.com>
2025-08-28 21:15:00 +0530

    [Updating code]

--- Java_8/_logicalCode.java
+++ Java_8/_logicalCode.java
@@ -1,5 +1,12 @@
 package Java_8;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 class vehicle {
 	protected String brand = "Maruti";
 
@@ -24,12 +31,14 @@ public class _logicalCode {
 		// ------- Virtual Thread -------
 		// virtualThreadExample();
 		// Thread.sleep(3000);
+		// ------- Thread Example -------
+		threadExample();
 	}
 
-	public void javaAttributeExample() {
+	public static void javaAttributeExample() {
 		_logicalCode cs = new _logicalCode();
 		cs.sampleValue = 25;
-		System.out.println(this.sampleValue);
+		// System.out.println(this.sampleValue);
 		System.out.println(cs.sampleValue);
 	}
 
@@ -37,6 +46,43 @@ public class _logicalCode {
 		Thread.startVirtualThread(() -> {
 			System.out.println("Hello this is a virtual thread!");
 		});
+	}
+
+	public static void threadExample() {
+		// Callable Example
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+
+		Callable<Integer> task = () -> {
+			Thread.sleep(5000);
+			return 42;
+		};
+
+		System.out.println("-- Calling the executor service --");
+		Future<Integer> future = executor.submit(task);
+		System.out.println("-- Calling the executor service End --");
+		try {
+			Integer result = future.get(); // Here the thread with execute and wait
+			System.out.println("Value from the callable Thread " + result);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		} finally {
+			executor.shutdown();
+		}
+
+		// Runnable Example - Execute on a main thread
+		Runnable runnableTask = () -> {
+			try {
+				Thread.sleep(1000);
+				System.out.println("Hi I am a task from runnable");
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		};
+		runnableTask.run();
+
+		new Thread(runnableTask).start(); // Execute asynchronously
 	}
 
 }
---
+++ Theory_Question.js
@@ -0,0 +1,25 @@
+let QuestionAsked = () => `
+
+1. How to return any value from the Java Thread
+- Use callable to return value
+
+2. Difference BW Runnable and callable
+- Runnable will simple run the function it will not return any value
+- callable will return the value
+
+3. Difference bw concurrent hasnmap and syncronized hashmap
+- null key is alloed in synchronized hashmap where as its not allowed in concurrent hashmap
+- S-map is slower instead of c-map is faster
+
+4. What is hashcollision
+- hash collision occurs when the 2 hashcode is equals
+- when a collision occurs, Java stores the colliding entries in a linked list.
+
+Hi Santosh,
+
+I hope you're doing well. Please find below my profile for the Java Full Stack role. I’ve attached my updated resume for your reference.
+
+Looking forward to hearing from you.
+
+Thanks & Regards, Hariom
+`;
